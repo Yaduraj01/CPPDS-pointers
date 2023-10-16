bd1a93f919ab181520c51c9cf4a74afd2a4b5565
Yaduraj01 <110488113+Yaduraj01@users.noreply.github.com>
2023-10-16 16:01:54 +0530

    Add files via upload

---
+++ lab_pointer.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+/*main()
+{
+    int i;
+    float fl;
+    int *int_ptr;
+    float *flt_ptr;
+    char *chr_ptr;
+
+    i = 5;
+    fl =3.4;
+    int_ptr = &i; // int_ptr contains the memory address of i
+    cout << "*int_ptr = " << *int_ptr ;
+    cout << "\n&i = " << &i;
+    cout << "\nint_ptr = " << int_ptr;
+    
+}*/
+
+/*To Do
+    1) comapre two strings using pointers
+    2) concatenate 2 strings
+    3) convert uppercase to lowercase
+*/
+
+//Call by Value exaple 
+/*main()
+{
+    int i,j;
+    i=2;
+    j=3;
+    cout << "Berfore swaping " << i<< " " << j;
+    swap(&i,&j);
+    cout << "\nAfter swap " << i << " " << j;
+}
+void swap (int*m, int*n)
+ {
+    int tmp;
+    tmp = *m;
+    *m = *n;
+    *n = tmp;
+    return ;
+}*/
